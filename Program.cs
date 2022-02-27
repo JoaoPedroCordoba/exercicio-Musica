bd94861fd8118309b0a199e258ea1fc771b63d2e
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-02-26 22:55:51 -0300

    finalizado exercicio-musica

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿Console.ForegroundColor =ConsoleColor.DarkYellow;
+Console.WriteLine("Bob Marley\n");
+Console.ResetColor();
+
+Console.ForegroundColor =ConsoleColor.DarkRed;
+Console.WriteLine("IS THIS LOVE\n");
+Console.ResetColor();
+
+Console.WriteLine("I want to love you\n");
+
+Console.WriteLine("I want to love you");
+Console.WriteLine("And treat you right");
+Console.WriteLine("I want to love you");
+Console.WriteLine("Every day and every night\n");
+
+Console.WriteLine("I want to love you");
+Console.WriteLine("And treat you right");
+Console.WriteLine("I want to love you");
+Console.WriteLine("Every day and every night\n");
+
+Console.WriteLine("We'll be together");
+Console.WriteLine("With a roof right over our heads, we'll share the shelter");
+Console.WriteLine("Of my single bed");
+Console.WriteLine("We'll share the same room\n");
+
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("Is this love, is this love, is this love, is this love that I'm feelin'?\n");
+Console.ResetColor();
+
+
+Console.WriteLine("I want to love you");
+Console.WriteLine("And treat you right");
+Console.WriteLine("I want to love you");
+Console.WriteLine("Every day and every night\n");
+
+Console.WriteLine("We'll be together");
+Console.WriteLine("With a roof right over our heads, we'll share the shelter");
+Console.WriteLine("Of my single bed");
+Console.WriteLine("We'll share the same room\n");
+
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("Is this love, is this love, is this love, is this love that I'm feelin'?\n");
+Console.ResetColor();
+
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("Is this love, is this love, is this love, is this love that I'm feelin'?\n");
+Console.ResetColor();
+
+Console.WriteLine("I wanna know, wanna know, wanna know now\n");
+
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("Is this love, is this love, is this love, is this love that I'm feelin'?\n");
+Console.ResetColor();
+
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("Is this love, is this love, is this love, is this love that I'm feelin'?\n");
+Console.ResetColor();
+
+
+Console.WriteLine("I want to love you");
